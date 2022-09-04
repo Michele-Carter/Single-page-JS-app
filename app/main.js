9799d10c0e06b5be0009c6c12d2939b10af6038f
Michele-Carter <103240106+Michele-Carter@users.noreply.github.com>
2022-09-04 21:38:18 +1200

    Add files via upload

--- app/main.js
+++ app/main.js
@@ -1,22 +1,39 @@
 $(document).ready(function () {
-    let minDate = new Date();
-    $("#depart").datepicker ({
-        showAnim: 'drop',
-        numberOfMonth: 1,
-        minDate: minDate,
-        dateFormat: 'dd/mm/yy',
-        onClose: function (selectedDate) {
-            $('#return').datepicker("option", "minDate", selectedDate);
-        }
-    })
+  let minDate = new Date();
+  $("#depart").datepicker({
+    showAnim: "drop",
+    numberOfMonth: 1,
+    minDate: minDate,
+    dateFormat: "dd/mm/yy",
+    onClose: function (selectedDate) {
+      $("#return").datepicker("option", "minDate", selectedDate);
+    },
+  });
 
-    $("#return").datepicker ({
-        showAnim: 'drop',
-        numberOfMonth: 1,
-        minDate: minDate,
-        dateFormat: 'dd/mm/yy',
-        onClose: function (selectedDate) {
-            $('#depart').datepicker("option", "maxDate", selectedDate);
-        }
-    })
-})
+  $("#return").datepicker({
+    showAnim: "drop",
+    numberOfMonth: 1,
+    minDate: minDate,
+    dateFormat: "dd/mm/yy",
+    onClose: function (selectedDate) {
+      $("#depart").datepicker("option", "maxDate", selectedDate);
+    },
+  });
+});
+
+/* hides results div by default on page load */
+function pageLoad() {
+  $(".results").hide();
+}
+
+pageLoad();
+
+/* on submit button click, hides pics and shows results instead */
+$(function () {
+  $("button").click(function () {
+    $(".options").remove();
+  });
+  $("button").click(function () {
+    $(".results").show();
+  });
+});
