8b6a1416bbe65aa2e9589fb404b370a2cd0032eb
Michele-Carter <103240106+Michele-Carter@users.noreply.github.com>
2022-09-07 13:14:05 +1200

    Add files via upload

--- app/main.js
+++ app/main.js
@@ -1,3 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+// get selected location option and listen for change
+$(document).ready(function(){
+	$("select").change(function(){
+		var myvalue = $(this).val();
+		alert("You have selected : "+myvalue);  //comment this out - do something else with value grabbed e.g. put into maps to calc distance
+	});
+});
+
+// get selected location option on button click
+$(document).ready(function(){
+	$("button").click(function(){
+		var myvalue3 = $('select').find(':selected').val();
+		alert("You have selected : "+myvalue3);  //comment this out - do something else with value grabbed e.g. put into maps to calc distance
+	});
+});
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+// date picker function
 $(document).ready(function () {
   let minDate = new Date();
   $("#depart").datepicker({
@@ -21,14 +47,21 @@ $(document).ready(function () {
   });
 });
 
-/* hides results div by default on page load */
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// hide results div by default on page load
 function pageLoad() {
   $(".results").hide();
 }
 
 pageLoad();
 
-/* on submit button click, hides pics and shows results instead */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// on submit button click, hides pics and shows results instead
 $(function () {
   $("button").click(function () {
     $(".options").remove();
@@ -37,3 +70,72 @@ $(function () {
     $(".results").show();
   });
 });
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/* map initialiser */
+function initMap() {
+    // map options
+    let mapOptions = {
+        zoom: 8,
+        center: {lat: -36.8509, lng: 174.7645},
+        mapTypeid: google.maps.MapTypeId.ROADMAP
+    };
+
+    // new map
+    let map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    // add marker
+    let marker = new google.maps.Marker({
+        position: {lat: -36.8509, lng: 174.7645},
+        map: map
+    })
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// create directions serive object to use the route method and get result
+var directionsService = new google.maps.DirectionsSercice();
+
+// create a DirectionsRender object which we will use to display the route
+var directionsRenderer = new google.maps.DirectionsRenderer();
+
+// bind the directions render to the map
+directionsRenderer.setMap(map);
+
+// function
+function calcRoute() {
+    // create request
+    let request = {
+        origin: document.getElementById("#origin").value,
+        destination: document.getElementById("#destination").value,
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+    }
+
+    // pass request to route method
+    DirectionsService.route(request, (result, status) => {
+        if (status == google.maps.DirectionsStatus.OK) {
+
+            // get distance and time
+            const output = document.querySelector("#output");
+            output.innerHTML = "<div class='alert-info'> from: " + document.getElementById("#origin").value + " + .<br />To: " + document.getElementById("#destination").value + ". <br /> Driving distance:" + result.routes[0].legs[0].distance.text + ".<br />Duration: " + result.routes[0].legs[0].duration.text + ". </div>";
+
+            // display route
+            directionsDisplay.setDirections(result);
+        } else {
+            // delete route from map
+            directionsDisplay.setDirections({ routes:[]});
+
+            // center map back to starting point
+            map.setCentre({lat: -36.8509, lng: 174.7645});
+
+            // show error message
+            output.innerHTML = "<div class='alert-danger'>Could not retrieve driving distance.</div>";
+        }
+    });
+}
+
